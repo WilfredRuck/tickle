51f4d17cf11b2d578dc2fe5632033a13c01a2111
Wilfred Ruck <wilfredoruck@gmail.com>
2019-05-14 18:40:58 -0400

    add testing

---
+++ spec/tickle_spec.js
@@ -0,0 +1,28 @@
+describe("tickle", () => {
+  var tickle = require('../lib/tickle');
+  const user1 = {name: "Kelly", age: 50, gender: "female", hc: "Allergies"};
+  const user2 = {name: "Josh", age: 40, gender: "male", hc: "Sleep Apnea"};
+  const user3 = {name: "Brad", age: 20, gender: "male", hc: "Heart Disease"};
+  const user4 = {name: "Anton", age: 18, gender: "male", hc: "Cyberchondria"};
+  const user5 = {name: "Wilfred", age: 10, gender: "male", hc: "Sleep Apnea"};
+
+  it("adds a discount for a female", () => {
+    expect(tickle(user1)).toEqual("Kelly, $210.20");
+  });
+
+  it("Applies health condition's cost increase", () => {
+    expect(tickle(user2)).toEqual("Josh, $190.80");
+  });
+
+  it("returns $117.00", () => {
+    expect(tickle(user3)).toEqual("Brad, $117.00");
+  });
+
+  it("doesn't increase the base price", () => {
+    expect(tickle(user4)).toEqual("Anton, $100.00");
+  });
+
+  it("returns a message when underage", () => {
+    expect(tickle(user5)).toEqual("Wilfred is not old enough! Come back in 8 year(s)");
+  });
+});
